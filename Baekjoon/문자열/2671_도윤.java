d3df6f658df1278e7734b89b06037d2d17caa841
Doyoon Lee <doyun7433@naver.com>
2022-01-13 20:32:04 +0900

    Create 2671_도윤.java

---
+++ Baekjoon/문자열/2671_도윤.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2671 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String ptn = "(100+1+|01)+";
+        System.out.println(str.matches(ptn) ? "SUBMARINE" : "NOISE");
+    }
+}
+
+// 정규식 이용
